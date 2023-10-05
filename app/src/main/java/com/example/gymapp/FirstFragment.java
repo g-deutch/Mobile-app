8a0baac2cb9cc7c63f56683af013ea0e5e302c43
paulfierman <paulfierman.biz@gmail.com>
2023-10-05 14:01:46 -0400

    Login page functionality

--- app/src/main/java/com/example/gymapp/FirstFragment.java
+++ app/src/main/java/com/example/gymapp/FirstFragment.java
@@ -11,9 +11,22 @@ import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.gymapp.databinding.FragmentFirstBinding;
 
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class FirstFragment extends Fragment {
 
     private FragmentFirstBinding binding;
+
+    //Some how need to extract map of user names and passwords from SignUpFragment
+    private final Map<String, String> users = new HashMap<>();
+
+    private EditText usernameEditText;
+    private EditText passwordEditText;
 
     @Override
     public View onCreateView(
@@ -30,19 +43,38 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        usernameEditText = view.findViewById(R.id.username_entry);
+        passwordEditText = view.findViewById(R.id.password_entry);
+        Button signInButton = view.findViewById(R.id.sign_in_button);
 
         binding.signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+                String username = usernameEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+
+                //line below allows sign in with no user or password
+                NavHostFragment.findNavController(FirstFragment.this).navigate(R.id.action_FirstFragment_to_SecondFragment);
+
+                //logic below requires valid user and password
+                /*
+               if (users.containsKey(username) && users.get(username).equals(password)) {
+                    Toast.makeText(getActivity(), "Sign in successful!", Toast.LENGTH_SHORT).show();
+                    NavHostFragment.findNavController(FirstFragment.this)
+                            .navigate(R.id.action_FirstFragment_to_SecondFragment);
+                } else {
+                    Toast.makeText(getActivity(), "Invalid username or password.", Toast.LENGTH_SHORT).show();
+                }
+                */
             }
         });
         binding.signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 NavHostFragment.findNavController(FirstFragment.this)
                         .navigate(R.id.action_FirstFragment_to_SignUpFragment);
+
             }
         });
     }
