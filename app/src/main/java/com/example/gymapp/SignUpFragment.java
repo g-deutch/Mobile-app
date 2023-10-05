4fc73eb3d5b90fc6c431c904e46f774c1528a623
paulfierman <paulfierman.biz@gmail.com>
2023-10-05 13:33:06 -0400

    Added username and password storage and error messages

--- app/src/main/java/com/example/gymapp/SignUpFragment.java
+++ app/src/main/java/com/example/gymapp/SignUpFragment.java
@@ -16,11 +16,66 @@ import com.example.gymapp.databinding.FragmentSettingsBinding;
 import com.example.gymapp.databinding.FragmentSignUpBinding;
 import com.example.gymapp.databinding.FragmentWorkoutsBinding;
 
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+
 
 public class SignUpFragment extends Fragment {
 
+    private FragmentSignUpBinding binding;
+    private static Map<String, String> users = new HashMap<>();
+    String username;
+    String password;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentSignUpBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
 
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
+        // Find UI elements by their IDs
+        EditText usernameEditText = view.findViewById(R.id.username_input);  // Updated ID
+        EditText passwordEditText = view.findViewById(R.id.password_input);  // Updated ID
+        Button signUpButton = view.findViewById(R.id.create_account_button);
+        Button backButton = view.findViewById(R.id.back_button5);
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                username = usernameEditText.getText().toString();
+                password = passwordEditText.getText().toString();
+                if(!password.equals("")) {
+                    if (users.containsKey(username)) {
+                        Toast.makeText(getActivity(), "Username already taken", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(getActivity(), "Account created successfully!", Toast.LENGTH_SHORT).show();
+                        users.put(username, password);
+
+                        NavHostFragment.findNavController(SignUpFragment.this)
+                                .navigate(R.id.action_SignUpFragment_to_FirstFragment);
+                        backButton.performClick();
+
+                    }
+                    usernameEditText.setText("");
+                    passwordEditText.setText("");
+                } else {
+                    Toast.makeText(getActivity(), "Must input password", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+/*
     private FragmentSignUpBinding binding;
 
     public SignUpFragment() {
@@ -44,5 +99,5 @@ public class SignUpFragment extends Fragment {
                         .navigate(R.id.action_SignUpFragment_to_FirstFragment);
             }
         });
-    }
+    } */
 }
